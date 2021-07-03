d32e884891b1437a0af2825bd609072127fb9c21
Justin <jforseth210@gmail.com>
2021-07-03 12:15:10 -0600

    Add 'add_calf' submission code

--- app/src/main/java/tech/jforseth/cattledatabase/cowAddCalfActivity.java
+++ app/src/main/java/tech/jforseth/cattledatabase/cowAddCalfActivity.java
@@ -2,19 +2,32 @@ package tech.jforseth.cattledatabase;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Switch;
+import android.widget.Toast;
 
 import org.json.JSONArray;
 import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class cowAddCalfActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Intent intent = getIntent();
+        String parentTagNumber = intent.getExtras().getString("tagNumber", "");
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cow_add_calf);
 
@@ -43,7 +56,7 @@ public class cowAddCalfActivity extends AppCompatActivity {
                         }
                     }
                     ArrayAdapter<String> sire_adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, parents);
-                    Spinner sireSpinner = findViewById(R.id.sireSpinner);
+                    Spinner sireSpinner = findViewById(R.id.addCalfSireSpinner);
                     sireSpinner.setAdapter(sire_adapter);
                 },
                 error -> {
@@ -52,8 +65,80 @@ public class cowAddCalfActivity extends AppCompatActivity {
                 this
         );
 
-        EditText ownerEditText = findViewById(R.id.editTextOwner);
+        EditText ownerEditText = findViewById(R.id.addCalfOwnerEditText);
         SharedPreferences preferences = getSharedPreferences("tech.jforseth.CattleDatabase", MODE_PRIVATE);
         ownerEditText.setText(preferences.getString("username", ""));
+
+        Button submitButton = findViewById(R.id.addCalfSubmitButton);
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DatePicker date = findViewById(R.id.addCalfDatePicker);
+                String year = String.valueOf(date.getYear());
+                String month = String.valueOf(date.getMonth() + 1);
+                String day = String.valueOf(date.getDayOfMonth());
+
+                if (month.length() < 2)
+                    month = "0" + month;
+                if (day.length() < 2)
+                    day = "0" + day;
+
+                String iso_date = year + "-" + month + "-" + day;
+
+                Switch bornEvent = findViewById(R.id.bornEventSwitch);
+                Switch calvedEvent = findViewById(R.id.calvedEventSwitch);
+
+                Boolean bornEventEnabled = bornEvent.isChecked();
+                Boolean calvedEventEnabled = calvedEvent.isChecked();
+
+                Spinner sireSpinner = findViewById(R.id.addCalfSireSpinner);
+                String sire = sireSpinner.getSelectedItem().toString();
+
+                EditText tagNumberEditText = findViewById(R.id.addCalfTagNumberEditText);
+                String tagNumber = tagNumberEditText.getText().toString().trim();
+
+                EditText addCalfOwnerEditText = findViewById(R.id.addCalfOwnerEditText);
+                String owner = addCalfOwnerEditText.getText().toString().trim();
+
+                Spinner sexSpinner = findViewById(R.id.addCalfSexSpinner);
+                String sex = sexSpinner.getSelectedItem().toString();
+
+                Map newCalfDict = new HashMap();
+                newCalfDict.put("dam", parentTagNumber);
+                newCalfDict.put("sire", sire);
+                newCalfDict.put("tag_number", tagNumber);
+                newCalfDict.put("owner", owner);
+                newCalfDict.put("sex", sex);
+                newCalfDict.put("born_event", bornEventEnabled);
+                newCalfDict.put("calved_event", calvedEventEnabled);
+                newCalfDict.put("date", iso_date);
+
+                JSONObject newCalfJSON = new JSONObject(newCalfDict);
+                addCalf(newCalfJSON);
+            }
+        });
+    }
+    public void addCalf(JSONObject newCalfJSON){
+        new makeHTTPRequest(
+                "add_cow",
+                newCalfJSON.toString(),
+                response -> {
+                    try {
+                        Toast.makeText(this, "Added " + newCalfJSON.getString("tag_number"), Toast.LENGTH_SHORT).show();
+                    } catch (JSONException e) {
+                        Toast.makeText(this, "Added calf", Toast.LENGTH_SHORT).show();
+                    }
+                    switchToMainActivity();
+                },
+                error -> {
+                    error.printStackTrace();
+                },
+                this
+        );
+    }
+    public void switchToMainActivity(){
+        Intent i = new Intent(this, MainActivity.class);
+        this.startActivity(i);
+        this.finish();
     }
 }
