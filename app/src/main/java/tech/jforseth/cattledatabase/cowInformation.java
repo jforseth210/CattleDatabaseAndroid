4879ea0946338fc3675bdd9594469da14e8602e1
Justin <jforseth210@gmail.com>
2021-07-10 22:09:15 -0600

    Add cow sex change

--- app/src/main/java/tech/jforseth/cattledatabase/cowInformation.java
+++ app/src/main/java/tech/jforseth/cattledatabase/cowInformation.java
@@ -5,8 +5,10 @@ import android.graphics.Paint;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -27,11 +29,11 @@ import java.util.Map;
 public class cowInformation extends AppCompatActivity {
     // Make sure to use the FloatingActionButton
     // for all the FABs
-    FloatingActionButton mAddFab, mAddParentFab, mAddCalfFab, mDeleteCowFab, mTransferOwnershipFab, mChangeTagNumberFab;
+    FloatingActionButton mAddFab, mAddParentFab, mAddCalfFab, mDeleteCowFab, mTransferOwnershipFab, mChangeTagNumberFab, mChangeSexFab;
 
     // These are taken to make visible and invisible along
     // with FABs
-    TextView mAddParentActionText, mAddCalfActionText, mDeleteCowActionText, mTransferOwnershipActionText, mChangeTagNumberActionText;
+    TextView mAddParentActionText, mAddCalfActionText, mDeleteCowActionText, mTransferOwnershipActionText, mChangeTagNumberActionText, mChangeSexActionText;
 
     // to check whether sub FAB buttons are visible or not.
     Boolean isAllFabsVisible;
@@ -61,6 +63,7 @@ public class cowInformation extends AppCompatActivity {
         mDeleteCowFab = findViewById(R.id.delete_cow_fab);
         mTransferOwnershipFab = findViewById(R.id.transfer_ownership_fab);
         mChangeTagNumberFab = findViewById(R.id.change_tag_number_fab);
+        mChangeSexFab = findViewById(R.id.change_sex_fab);
 
         // Also register the action name text, of all the FABs.
         mAddParentActionText = findViewById(R.id.add_parent_action_text);
@@ -68,6 +71,7 @@ public class cowInformation extends AppCompatActivity {
         mDeleteCowActionText = findViewById(R.id.delete_cow_action_text);
         mTransferOwnershipActionText = findViewById(R.id.transfer_ownership_action_text);
         mChangeTagNumberActionText = findViewById(R.id.change_tag_number_action_text);
+        mChangeSexActionText = findViewById(R.id.change_sex_action_text);
 
         // Now set all the FABs and all the action name
         // texts as GONE
@@ -75,11 +79,13 @@ public class cowInformation extends AppCompatActivity {
         mAddCalfFab.setVisibility(View.GONE);
         mDeleteCowFab.setVisibility(View.GONE);
         mTransferOwnershipFab.setVisibility(View.GONE);
+        mChangeSexFab.setVisibility(View.GONE);
         mChangeTagNumberFab.setVisibility(View.GONE);
         mAddParentActionText.setVisibility(View.GONE);
         mAddCalfActionText.setVisibility(View.GONE);
         mDeleteCowActionText.setVisibility(View.GONE);
         mChangeTagNumberActionText.setVisibility(View.GONE);
+        mChangeSexActionText.setVisibility(View.GONE);
         mTransferOwnershipActionText.setVisibility(View.GONE);
 
         // make the boolean variable as false, as all the
@@ -101,11 +107,13 @@ public class cowInformation extends AppCompatActivity {
                         mDeleteCowFab.show();
                         mTransferOwnershipFab.show();
                         mChangeTagNumberFab.show();
+                        mChangeSexFab.show();
                         mAddParentActionText.setVisibility(View.VISIBLE);
                         mAddCalfActionText.setVisibility(View.VISIBLE);
                         mDeleteCowActionText.setVisibility(View.VISIBLE);
                         mTransferOwnershipActionText.setVisibility(View.VISIBLE);
                         mChangeTagNumberActionText.setVisibility(View.VISIBLE);
+                        mChangeSexActionText.setVisibility(View.VISIBLE);
 
                         // make the boolean variable true as
                         // we have set the sub FABs
@@ -121,11 +129,12 @@ public class cowInformation extends AppCompatActivity {
                         mDeleteCowFab.hide();
                         mTransferOwnershipFab.hide();
                         mChangeTagNumberFab.hide();
+                        mChangeSexFab.hide();
                         mAddParentActionText.setVisibility(View.GONE);
                         mAddCalfActionText.setVisibility(View.GONE);
                         mDeleteCowActionText.setVisibility(View.GONE);
                         mTransferOwnershipActionText.setVisibility(View.GONE);
-                        mChangeTagNumberActionText.setVisibility(View.GONE);
+                        mChangeSexActionText.setVisibility(View.GONE);
 
                         // make the boolean variable false
                         // as we have set the sub FABs
@@ -199,6 +208,65 @@ public class cowInformation extends AppCompatActivity {
                                     AlertDialog success_dialog = new AlertDialog.Builder(this)
                                             .setTitle("Tag Number Changed")
                                             .setMessage(tagNumber + " is now " + new_tag)
+                                            .setPositiveButton("Ok", null)
+                                            .create();
+                                    success_dialog.show();
+                                },
+                                error -> {
+                                    error.printStackTrace();
+                                },
+                                this
+                        );
+                    })
+                    .create();
+            dialog.show();
+
+        });
+        mChangeSexFab.setOnClickListener(v -> {
+            final Spinner sexSpinner = new Spinner(this);
+
+            new makeHTTPRequest(
+                    "cows/sex_list",
+                    "",
+                    sex_list_response -> {
+                        try {
+                            JSONArray sexes_json = sex_list_response.getJSONArray("sexes");
+                            String[] sexes = new String[sexes_json.length()];
+                            for (int i = 0; i < sexes_json.length(); i++){
+                                sexes[i] = sexes_json.getString(i);
+                            }
+                            ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, sexes);
+                            sexSpinner.setAdapter(adapter);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    },
+                    error -> {
+
+                    },
+                    this
+            );
+            AlertDialog dialog = new AlertDialog.Builder(this)
+                    .setTitle("Change Sex")
+                    .setView(sexSpinner)
+                    .setNegativeButton("Cancel", (dialog1, which) -> {
+                        //Do nothing
+                    })
+                    .setPositiveButton("Change", (dialog1, which) -> {
+                        String sex = sexSpinner.getSelectedItem().toString();
+                        String tag_number = tagNumber;
+                        Map newTagDict = new HashMap();
+                        newTagDict.put("tag_number", tag_number);
+                        newTagDict.put("sex", sex);
+                        JSONObject newTagJSON = new JSONObject(newTagDict);
+
+                        new makeHTTPRequest(
+                                "cows/change_sex",
+                                newTagJSON.toString(),
+                                chang_tag_response -> {
+                                    AlertDialog success_dialog = new AlertDialog.Builder(this)
+                                            .setTitle("Sex Changed")
+                                            .setMessage(tagNumber + " is now a " + sex)
                                             .setPositiveButton("Ok", null)
                                             .create();
                                     success_dialog.show();
